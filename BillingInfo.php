187b3c6d780236f14da7b823ed41badcabd91e5c
cir0002 <35436134+cir0002@users.noreply.github.com>
2018-03-18 19:11:30 -0500

    BillingInfoTab

--- BillingInfo.php
+++ BillingInfo.php
@@ -1,106 +1,144 @@
 <!--This PHP code is for the billing tab on the main page, it includes the HTML and CSS-->
-<style>
-   .inlinetable
-   {
-       display: inline-block;
-   }
-    /* Table */
-    .inlinetable {
-            border-collapse: collapse;
-            font-size: 14px;
-            min-width: 537px;
-    }
+<!--CSS for my code -->
 
-    .inlinetable th, 
-    .inlinetable td {
-            border: 1px solid #e1edff;
-            padding: 7px 17px;
-    }
-    .inlinetable caption {
-            margin: 7px;
-    }
-    /* Table Header */
-    .inlinetable thead th {
-            background-color: #508abb;
-            color: #FFFFFF;
-            border-color: #6ea1cc !important;
-            text-transform: uppercase;
-    }
+<link rel="stylesheet" href="css/tablestyle.css">
+<table style="display: inline-block;">
 
-    /* Table Body */
-    .inlinetable tbody td {
-            color: #353535;
-    }
-    .inlinetable tbody td:first-child,
-    .inlinetable tbody td:nth-child(4),
-    .inlinetable tbody td:last-child {
-            text-align: right;
-    }
+<!--HTML & PHP Code for my tab-->
 
-    .inlinetable tbody tr:nth-child(odd) td {
-            background-color: #f4fbff;
-    }
-    .inlinetable tbody tr:hover td {
-            background-color: #ffffa2;
-            border-color: #ffff0f;
-    }
+<!-- Adding payment -->
 
-    /* Table Footer */
-    .inlinetable tfoot th {
-            background-color: #e5f5ff;
-            text-align: right;
-    }
-    .inlinetable tfoot th:first-child {
-            text-align: left;
-    }
-    .inlinetable tbody td:empty
-    {
-            background-color: #ffcccc;
-    }
-</style>
+<title>Amount being paid:</title>
+<link rel="stylesheet" href="css/tablestyle.css">
+<link rel="stylesheet" type="text/css" href="mainpage.css"/>
+</head> 
+<br>
+<br>
+<center>
+        <form id="search-form" method="post">
+          <table border="0.5" class="data-table">
+            <tr>
+                <!--<td><strong><label for="user_id"><center>Payment Method</label></strong></td>
+                <td><select name="searchType">
+                <option value="DEBIT">Debit Card</option>
+                <option value="CHECK">Check</option>
+                <option value="CASH">Cash</option>
+                 </select>
+                </td>-->
+                <td><input type="p_text" name="payment" id="payment"></center></td>
+                <td><input type="submit" value="Submit Payment" />		
+            </tr>
+           </table>
+    </form>
+</center>
+<?php
+session_start();
+require("db_connect.php");
+    
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $amtpaid = $_POST['payment'];
+    $query = "Select AmtPaid From InsuranceInfo where PatientID = '$_SESSION[p_id]'";
+    $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+    
+    $row = mysqli_fetch_array($result);
+    $money = $row[0];
+    $totalmoney = $money+$amtpaid;
+    $sql = "UPDATE InsuranceInfo SET AmtPaid = $totalmoney WHERE PatientId = '$_SESSION[p_id]'";
+    
+    $result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
+}
+?>
+<script type="text/javascript">
+function callFunction(clicked_id){
+  window.location.href = "serverScript.php?pid="+clicked_id;
+}
+</script>
+   
 
-<!--HTML Code for my tab-->
-
-<table class="BillingInfo">
-<caption class="title">Itemized List</caption>
-<thead>
-        <tr>
-            <th>Amount Paid by Insurance</th>
-            <th>Amount Paid by Patient</th>        
-        </tr>"
-</thead>
-<tbody>
-
-<table class="BillingInfo">
+<table class="data-table" style="display: inline-block; float: left;">
 <caption class="title">Itemized List</caption>
 <thead>
         <tr>
             <th>Items</th>
             <th>Cost</th>        
-        </tr>"
+        </tr>
 </thead>
-<tbody>
- <?php
-    require("db_connect.php");
+
+
+<?php
+    
+    $no = 1;
+    $total = 0;
+    
     $res = mysqli_query($connection, "Select * FROM ItemizedList WHERE PatientID = '$_SESSION[p_id]'"); 
     while($row = mysqli_fetch_array($res))
     {
+        $amount  = $row['Cost'] == 0 ? '' : number_format($row['Cost']);
+        
         echo "<tr>";
         echo "<td>" . $row['Item'] . "</td>";
         echo "<td>" . $row['Cost'] . "</td>";
         echo "</tr>";
+        $total += $row['Cost'];
+        $no++;      
     }
-       
-    $res = mysqli_query($connection, "Select * FROM InsuranceInfo  WHERE PatientID = '$_SESSION[p_id]'");    
-    while($row = mysqli_fetch_array($res))
-    {
-        echo '<tr>
-            <td>' . $row['AmtPaidByInsurance'] . '</td>
-            <td>' . $row['AmtPaidByPatient'] . '</td>"
-        </tr>';
+ ?>
+        </tbody>
+        <tfoot>
+            <tr>
+                    <th colspan="1">TOTAL</th>
+                    <th><?=number_format($total)?></th>
+            </tr>
+        </tfoot>
+<tbody>
+
+<table class="data-table" style="display: inline-block;">
+<caption class="title">Payment</caption>
+<thead>
+        <tr>
+            <th>Amount Paid by Insurance:</th>
+            <th>Amount due after Insurance:</th> 
+        </tr>
+</thead>
+ <?php
+    $payment=0;
+    $res1 = mysqli_query($connection, "Select * FROM InsuranceInfo  WHERE PatientID = '$_SESSION[p_id]'");
+    
+    $query2 = "Select AmtPaidByInsurace From InsuranceInfo where PatientID = '$_SESSION[p_id]'";
+    $res2 = mysqli_query($connection, $query2) or die(mysqli_error($connection));
+    $row2 = mysqli_fetch_array($res2);
+    $amtmoney = $row2[0];
+    
+    while($row = mysqli_fetch_array($res1))
+    {     
+        echo "<tr>";
+        echo "<td>" . $row['AmtPaidByInsurance'] . "</td>";
+        
+        echo "<td>" . $row['AmtDueAfterInsurance'] . "</td>";
+        echo "</tr>";
+        $query = "SELECT `AmtPaid` FROM `InsuranceInfo` WHERE PatientID = '$_SESSION[p_id]'";
+        
+        $payment=$payment + $row['AmtPaidByInsurance'] + $row['AmtPaid'];
+        $amtdue = $total - $payment;
     }
     
     mysqli_close($connection);
-    
 ?>
+</tbody>
+    <tfoot>
+    <tr>
+        <th colspan="1">Amount Paid:</th>
+        <th><?php echo $totalmoney?></th>
+    </tr>
+    <tr>
+        <th colspan="1">Amount Due:</th>
+        <th><?php echo $amtdue?></th>
+    </tr>    
+    </tfoot>
+<tbody>
+
+
+    
+
 
