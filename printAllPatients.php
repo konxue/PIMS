19985f13dd8655d79250177cf71e838df3e6c56a
cir0002 <35436134+cir0002@users.noreply.github.com>
2018-04-08 18:49:37 -0500

    printstufff

--- printAllPatients.php
+++ printAllPatients.php
@@ -7,8 +7,7 @@
                 <table border="0.5" class="data-table">
                     <center>  <caption class="title"><center>Print Options</caption> </center>
                         <tr>
-                            <td><input type="submit" name="export" value="Print All Patients" /></td>
-                            <td><input type="submit" name="export2" value="Print Patient Summary"/></td>             
+                            <td><input type="submit" name="export" value="Print Summary Report on all patients" /></td>                                       
                         </tr>
                     </table>
                 </form>';
@@ -69,73 +68,4 @@
         }
     }
     
-    //print current patients summary
-    $p_id = $_SESSION['p_id'];
-    
-    $output2 = '';
-    if(isset($_POST["export2"]))
-    {
-        $sql = "SELECT * FROM `PatientInfo` INNER JOIN Procedures INNER JOIN Prescriptions INNER JOIN MedicalInfo INNER JOIN DoctorsNote ON brand.brand_id = product.brand_id"; 
-        $query = "SELECT * FROM `PatientInfo` WHERE `PatientID` = '$p_id'";
-        $query1 = "SELECT `Name`, `Date`, `Time` FROM Procedures WHERE PatientID = '$p_id'";
-        $query2 = "SELECT `PrescripName`, `Dosage`, `Quantity`, `Directions` FROM Prescription WHERE `PatientID` = '$p_id'";
-        $query3 = "SELECT `AdmissionDate`, `AdmissionTime`, `ReasonForAdmission`, `DischargeTime` FROM MedicalInfo WHERE `PatientID` = '$p_id'";
-        $query4 = "SELECT `Note` FROM DoctorsNote WHERE `PatientID` = '$p_id'";
-        $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
-        $result1 = mysqli_query($connection, $query1) or die(mysqli_error($connection));
-        $result2 = mysqli_query($connection, $query2) or die(mysqli_error($connection));
-        $result3 = mysqli_query($connection, $query3) or die(mysqli_error($connection));
-        $result4 = mysqli_query($connection, $query4) or die(mysqli_error($connection));
-        if(mysqli_num_rows($result) > 0)
-        {
-            $output2 .= '
-                <table class="table" bordered="1">  
-                    <tr>  
-                        <th>Patient ID</th>
-                        <th>Last Name</th>
-                        <th>Middle Initial</th>
-                        <th>First Name</th>
-                        <th>Date of Birth</th>
-                        <th>Street</th>
-                        <th>City</th>
-                        <th>State</th>
-                        <th>ZIP</th>
-                        <th>Home Phone</th>
-                        <th>Mobile Phone</th>
-                        <th>Sex</th>                    
-                        <th>AdmissionDate</th>
-                        <th>AdmissionTime</th>
-                        <th>Reason for Admission</th>
-                        <th>Discharge Time</th>
-                        <th>Notes</th>
-                    </tr>
-            ';
-            while($row = mysqli_fetch_array($result))
-            {
-                $output2 .= '
-                    <tr>  
-                        <td><center>'.$row['PatientID'].'</center></td>
-                        <td><center>'.$row['LastName'].'</center></td>
-                        <td><center>'.$row['MiddleName'].'</center></td> 
-                        <td><center>'.$row['FirstName'].'</center></td> 
-                        <td><center>'.$row['DOB'].'</center></td>
-                        <td><center>'.$row['Street'].'</center></td>
-                        <td><center>'.$row['City'].'</center></td>
-                        <td><center>'.$row['State'].'</center></td>
-                        <td><center>'.$row['ZIP'].'</center></td>
-                        <td><center>'.$row['HomePhone'].'</center></td>
-                        <td><center>'.$row['MobilePhone'].'</center></td>
-                        <td><center>'.$row['Sex'].'</center></td>
-                        <td><center>'.$row['AdmissionDate'].'</center></td>
-                        <td><center>'.$row['AdmissionTime'].'</center></td>
-                        <td><center>'.$row['ReasonforAdmission'].'</center></td>
-                        <td><center>'.$row['DischargeTime'].'</center></td>                        
-                        <td><center>'.$row['Notes'].'</center></td>
-                    </tr>';
-            }
-            $output2 .= '</table>';
-            $_SESSION['printOut'] = $output2;
-            echo '<meta http-equiv="refresh" content="0; url=printreport.php" />'; 
-        }
-    }   
 ?>
